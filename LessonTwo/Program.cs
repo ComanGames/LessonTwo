34e2f1cd3e6ca791f4adaaafc067904a8f5e5ca8
ComanGames <alfaa.gen@gmail.com>
2016-04-02 19:38:46 +0300

    First part

--- LessonTwo/Program.cs
+++ LessonTwo/Program.cs
@@ -1,10 +1,30 @@
 ﻿
+using System;
+
 namespace LessonTwo
 {
     class Program
     {
         static void Main()
         {
+            FirstAboutVariables();
+            Console.ReadKey();
+        }
+
+        private static void FirstAboutVariables()
+        {
+            int x = 5;
+            float y = 5.3f;
+            char c = 'c';
+            string s = "Hello";
+            c = s[0];
+            Console.WriteLine(c);
+            c = s[1];
+            Console.WriteLine(c);
+            c = s[2];
+            Console.WriteLine(c);
+            c = s[3];
+            Console.WriteLine(c);
         }
     }
 }
