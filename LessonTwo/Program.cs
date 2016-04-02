95d6e2b07455f77f49cccc6ba4ce14841acdf58f
Галица Олександр <oles.glts@gmail.com>
2016-04-02 19:40:20 +0300

    New changes

--- LessonTwo/Program.cs
+++ LessonTwo/Program.cs
@@ -7,24 +7,7 @@ namespace LessonTwo
     {
         static void Main()
         {
-            FirstAboutVariables();
-            Console.ReadKey();
-        }
-
-        private static void FirstAboutVariables()
-        {
-            int x = 5;
-            float y = 5.3f;
-            char c = 'c';
-            string s = "Hello";
-            c = s[0];
-            Console.WriteLine(c);
-            c = s[1];
-            Console.WriteLine(c);
-            c = s[2];
-            Console.WriteLine(c);
-            c = s[3];
-            Console.WriteLine(c);
+            Console.WriteLine("Hello");
         }
     }
 }
