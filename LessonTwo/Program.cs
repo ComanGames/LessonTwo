e015485c99f98d34a8b9dc746b13cd78892b3332
ComanGames <alfaa.gen@gmail.com>
2016-04-02 19:35:30 +0300

    I cleaned the project

--- LessonTwo/Program.cs
+++ LessonTwo/Program.cs
@@ -1,25 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace LessonTwo
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-        }
-
-        private static void SomeNewMethod()
-        {
-            Console.WriteLine("Hello Yura   ");
-            Console.WriteLine("Hello Sasha");
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine("hello");
-            }
         }
     }
 }
