9018c2a882fd8a3b0376b838d448a7ff7049b454
ComanGames <alfaa.gen@gmail.com>
2016-04-02 19:41:12 +0300

    New changes

--- LessonTwo/Program.cs
+++ LessonTwo/Program.cs
@@ -7,7 +7,24 @@ namespace LessonTwo
     {
         static void Main()
         {
-            Console.WriteLine("Hello");
+            FirstAboutVariables();
+            Console.ReadKey();
+        }
+
+        private static void FirstAboutVariables()
+        {
+            int x = 5;
+            float y = 5.3f;
+            char c = 'c';
+            string s = "Hello";
+            c = s[0];
+            Console.WriteLine(c);
+            c = s[1];
+            Console.WriteLine(c);
+            c = s[2];
+            Console.WriteLine(c);
+            c = s[3];
+            Console.WriteLine(c);
         }
     }
 }
