8816fc3feedcc75ca5a59a44284d9519265cee62
Галица Олександр <oles.glts@gmail.com>
2016-04-09 17:40:13 +0300

    changes

--- LessonTwo/Program.cs
+++ LessonTwo/Program.cs
@@ -19,7 +19,7 @@ namespace LessonTwo
             //true false &&  false
             //false false &&  false
             //false true &&  false
-            //true true &&  false
+            //true true &&  true
             //true || false  true
             //false || true  true
             //true || true true 
